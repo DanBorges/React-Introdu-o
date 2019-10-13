bc215b91d04a7ad2db1bbf3db891b79ff4518b02
Daniel Borges <daniel.lemes.borges@hmail.com>
2019-10-12 22:57:14 -0300

    Criando primeiros componentes

---
+++ src/componentes/FamiliaSilva.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Membro from './Membro'
+
+
+export default props => 
+    <div>
+        <Membro nome ="Rafael" sobrenome="silva"></Membro>
+        <Membro nome ="Ana" sobrenome="silva"></Membro>
+        <Membro nome ="Raquel" sobrenome="silva"></Membro>
+        <Membro nome ="Marcos" sobrenome="silva"></Membro>
+
+    </div>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,52 @@ import React from 'react'
 import ReactDom from 'react-dom';
 import PrimeiroComponente from './componentes/PrimeiroComponente'
 
-// const elemento = document.getElementById('root')
-// ReactDom.render(<h1>Olá React JSX</h1>, elemento)
+import {CompA, CompB} from './componentes/DoisComponentes'
+
+import FamiliaSilva from './componentes/FamiliaSilva'
+
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
-// ReactDom.render(
-//     <PrimeiroComponente/>, 
+// ReactDom.render(<h1>Olá React JSX</h1>, elemento)
+
+// const elemento = document.getElementById('root')
+// // ReactDom.render(
+// //     <PrimeiroComponente/>, 
+// //     elemento)
+
+// ReactDom.render(    
+//     <PrimeiroComponente
+//         nome="Daniel"
+//         numero={45}
+//     />, 
 //     elemento)
 
-ReactDom.render(    
-    <PrimeiroComponente
-        nome="Daniel"
-        numero={45}
-    />, 
-    elemento)
+// ReactDom.render(
+//     <div>
+//         <CompA valor= "oi eu sou o A"/>
+//         <CompB valor="Aqui é o B"/>
+//     </div>
+//     , elemento
+// )
+
+// ReactDom.render(
+//     <div>
+//         <FamiliaSilva></FamiliaSilva>
+//     </div>
+//     , elemento
+
+ReactDom.render(
+    <div>
+        <Familia>
+            <Membro nome="Daniela" sobrenome="Marques"></Membro>
+            <Membro nome="Danuza" sobrenome="Marques"></Membro>
+        </Familia>
+        <Familia>
+            <Membro nome="Fernando" sobrenome="Pereira"></Membro>
+            <Membro nome="Gabriel" sobrenome="Pereira"></Membro>
+        </Familia>
+    </div>
+    , elemento
+)
